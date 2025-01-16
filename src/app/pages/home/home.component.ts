7a6daaac22cc186407b8e3aba1cc08ed418ace12
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2025-01-15 21:15:29 -0600

    feat: Update files

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
+import { AuthService } from 'src/app/services/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HomeComponent {
-
+  constructor(private authService: AuthService) {}
 }
